eda225ddec1b3a233078115cba8bd61cb0c0259d
christopher-besch <mail@chris-besch.com>
2025-01-24 22:50:49 +0100

    initial commit

---
+++ src/client/java/com/chrisbesch/mcmissile/MCMissileClient.java
@@ -0,0 +1,10 @@
+package com.chrisbesch.mcmissile;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCMissileClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
