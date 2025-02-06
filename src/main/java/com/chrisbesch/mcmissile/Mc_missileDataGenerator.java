cbafca594a1aded0831a428cd95830e2ea3d9461
christopher-besch <mail@chris-besch.com>
2025-02-06 08:25:18 +0100

    feat: format fabric template java files

--- src/main/java/com/chrisbesch/mcmissile/Mc_missileDataGenerator.java
+++ src/main/java/com/chrisbesch/mcmissile/Mc_missileDataGenerator.java
@@ -4,8 +4,6 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class Mc_missileDataGenerator implements DataGeneratorEntrypoint {
-	@Override
-	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
-	}
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {}
 }
